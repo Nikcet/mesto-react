f06e3aea3d94334310c140420938118b8ee80c24
Nikcet <mstislav.kr@yandex.ru>
2022-01-13 16:42:24 +0300

    Восстановил плавность открытия попапа

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,9 +1,9 @@
 export default function ImagePopup(props) {
   return (
-    props.card && <div id="popupImage" className={`image-popup popup ${props.card ? "popup_opened" : ""}`}>
+    <div id="popupImage" className={`image-popup popup ${props.card ? "popup_opened" : ""}`}>
       <div className="image-popup__wrap">
-        <img src={props.card.link} alt={props.card.name} className="image-popup__image" />
-        <p className="image-popup__description">{props.card.name}</p>
+        <img src={props.card?.link} alt={props.card?.name} className="image-popup__image" />
+        <p className="image-popup__description">{props.card?.name}</p>
         <button
           className="image-popup__close-btn popup__close"
           type="button"
